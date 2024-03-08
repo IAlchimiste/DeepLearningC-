b2a03ec431d4648b46e15e369904b2eb58ba0372
The IAlchimiste <ialchimiste@debian.lan>
2024-03-08 19:51:25 +0100

    add function to get training data from file

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,69 @@
 #include <iostream>
 #include "NN.h"
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+// Fonction pour ouvrir un fichier et lire sont contenu pour le mettre dans un vecteur
+vector<float> readFile(string filename) {
+    vector<float> data;
+    ifstream file(filename);
+
+    if (file.is_open()) {
+        string line;
+        while (getline(file, line)) {
+            data.push_back(stof(line));
+        }
+    } else {
+        cout << "Impossible d'ouvrir le fichier" << endl;
+    }
+
+    return data;
+}
+
+// Fonction pour charger plusieur fichier dans un vecteur
+vector<vector<float>> loadFiles(vector<string> filenames) {
+    vector<vector<float>> data;
+
+    for (string &filename : filenames) {
+        data.push_back(readFile(filename));
+    }
+
+    return data;
+}
+
+vector<string> listFiles(string folder) {
+    vector<string> filenames;
+    int i = 1;
+
+    while (true) {
+        string filename = folder + "/" + to_string(i) + ".txt";
+        ifstream ifile(filename);
+
+        if (!ifile) {
+            break;
+        }
+
+        filenames.push_back(filename);
+        i++;
+    }
+
+    return filenames;
+}
+
+void loadDataFromFile(vector<vector<float>> &inputs, vector<vector<float>> &desiredOutputs) {
+    vector<string> listInputFile = listFiles("data/inputs");
+    vector<string> listDesiredOutputFile = listFiles("data/outputs");
+
+    cout << "Inputs  file number : " << listInputFile.size() << endl;
+    cout << "Outputs file number : " << listDesiredOutputFile.size() << endl;
+
+    inputs = loadFiles(listInputFile);
+    desiredOutputs = loadFiles(listDesiredOutputFile);
+}
 
 int main() {
-    srand(time(0));
-
     int numInputs = 2;
     int numOutputs = 2;
     int numHiddenLayers = 5;
@@ -14,29 +74,27 @@ int main() {
     vector<float> inputValues = {1, 0};
     vector<float> output = nn.feedForward(inputValues);
 
-    cout << "Sortie non entrainée : " << endl;
 
-    for (float &i : output) {
-        cout << i << endl;
+    vector<vector<float>> inputs;
+    vector<vector<float>> desiredOutputs;
+
+    loadDataFromFile(inputs, desiredOutputs);
+
+    for (int j = 0; j < 1000; j++) {
+        for (int i = 0; i < inputs.size(); i++) {
+            output = nn.feedForward(inputs[i]);
+            nn.backPropagation(desiredOutputs[i], nn.feedForward(inputs[i]), 0.1);
+        }
     }
 
-    vector<float> expectedOutput = {1, 0};
+    output = nn.feedForward(inputValues);
 
-    for (int i = 0; i< 500; i++) {
-        nn.backPropagation(expectedOutput, output, 0.1);
-        output = nn.feedForward(inputValues);
+    cout << "Output : " ;
+
+    for (float &value : output) {
+        cout << value << " ";
     }
-    cout << "sortie souhaiter : " << endl;
-
-    for (float &i : expectedOutput) {
-        cout << i << endl;
-    } 
-    
-    cout << "Sortie entrainée : " << endl;
-
-    for (float &i : output) {
-        cout << i << endl;
-    }
+    cout << endl;
 
     return 0;
 }
